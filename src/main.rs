5a3aa3e5242205963590f31c7a6494103b60fa69
Charlie Somerville <charlie@charlie.bz>
2019-11-19 21:14:49 +1100

    hook up coro type

--- src/main.rs
+++ src/main.rs
@@ -1,73 +1,18 @@
-use std::cell::RefCell;
-use std::collections::VecDeque;
-use std::future::Future;
-use std::pin::Pin;
-use std::task::{Context, Poll};
-
 mod waker;
 
-struct Chan {
-    items: RefCell<VecDeque<i32>>,
-}
+use waker::{Coro, Chan};
 
-impl Chan {
-    pub fn new() -> Self {
-        Chan { items: RefCell::new(VecDeque::new()) }
-    }
-
-    pub fn send(&self, value: i32) {
-        self.items.borrow_mut().push_front(value)
-    }
-
-    pub fn recv(&self) -> ChanRecv<'_> {
-        ChanRecv { src: self }
-    }
-}
-
-struct ChanRecv<'a> {
-    src: &'a Chan,
-}
-
-impl<'a> Future for ChanRecv<'a> {
-    type Output = i32;
-
-    fn poll(self: Pin<&mut Self>, _cx: &mut Context) -> Poll<i32> {
-        match self.src.items.borrow_mut().pop_front() {
-            None => Poll::Pending,
-            Some(x) => Poll::Ready(x),
-        }
-    }
-}
-
-async fn get(src: &Chan) -> i32 {
-    src.recv().await
-}
-
-async fn add(a: i32, b: i32) -> i32 {
+async fn coro(mut chan: Chan) -> i32 {
+    let a = chan.recv().await;
+    let b = chan.recv().await;
     a + b
 }
 
-async fn foo(src: &Chan) -> i32 {
-    add(get(src).await, get(src).await).await
-}
-
 fn main() {
-    let chan = Chan::new();
+    let coro = Coro::run(coro);
 
-    let fut = foo(&chan);
+    let mut pin_coro = Box::pin(coro);
 
-    let mut pf = Box::pin(fut);
-
-    let waker = waker::waker();
-    let mut ctx = Context::from_waker(&waker);
-
-    println!("{:?}", pf.as_mut().poll(&mut ctx));
-
-    chan.send(123);
-
-    println!("{:?}", pf.as_mut().poll(&mut ctx));
-
-    chan.send(456);
-
-    println!("{:?}", pf.as_mut().poll(&mut ctx));
+    println!("{:?}", pin_coro.as_mut().advance(123));
+    println!("{:?}", pin_coro.as_mut().advance(456));
 }
--- src/waker.rs
+++ src/waker.rs
@@ -1,5 +1,9 @@
+use std::cell::RefCell;
+use std::future::Future;
+use std::pin::Pin;
 use std::ptr;
-use std::task::{Waker, RawWaker, RawWakerVTable};
+use std::rc::Rc;
+use std::task::{Context, Waker, RawWaker, RawWakerVTable, Poll};
 
 mod methods {
     use std::task::RawWaker;
@@ -30,4 +34,52 @@ static RAW_WAKER_VTABLE: RawWakerVTable = RawWakerVTable::new(
 
 pub fn waker() -> Waker {
     unsafe { Waker::from_raw(RawWaker::new(ptr::null(), &RAW_WAKER_VTABLE)) }
+}
+
+pub struct Chan {
+    cell: Rc<RefCell<Option<i32>>>,
+}
+
+impl Chan {
+    pub fn recv(&mut self) -> ChanRecv<'_> {
+        ChanRecv { chan: self }
+    }
+}
+
+pub struct ChanRecv<'a> {
+    chan: &'a mut Chan,
+}
+
+impl<'a> Future for ChanRecv<'a> {
+    type Output = i32;
+
+    fn poll(self: Pin<&mut Self>, _ctx: &mut Context) -> Poll<i32> {
+        match self.chan.cell.borrow_mut().take() {
+            Some(x) => Poll::Ready(x),
+            None => Poll::Pending,
+        }
+    }
+}
+
+pub struct Coro<Fut> {
+    cell: Rc<RefCell<Option<i32>>>,
+    fut: Fut,
+}
+
+impl<Fut> Coro<Fut> where Fut: Future<Output = i32> {
+    pub fn run(f: impl FnOnce(Chan) -> Fut) -> Self {
+        let cell = Rc::new(RefCell::new(None));
+
+        let fut = f(Chan { cell: cell.clone() });
+
+        Coro { cell, fut }
+    }
+
+    pub fn advance(self: Pin<&mut Self>, value: i32) -> Poll<i32> {
+        *self.cell.borrow_mut() = Some(value);
+        let waker = waker();
+        let mut ctx = Context::from_waker(&waker);
+        let pin_fut = unsafe { self.map_unchecked_mut(|c| &mut c.fut) };
+        Fut::poll(pin_fut, &mut ctx)
+    }
 }
